864e75e6ebbdb4be6fe986f00aeef35ad71b350f
kunkuliuke <aurepavi@gmail.com>
2013-07-09 17:52:20 +0000

    jau

--- web.js
+++ web.js
@@ -3,11 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var buf = new buffer();
 var fs = require('fs');
 
+var mybuf =  fs.readFileSync('index.html');
 
-  response.send(buf.toString('utc 8', fs.readFileSync("index.html")));
+
+
+  response.send(mybuf.toString());
 });
 
 var port = process.env.PORT || 5000;
